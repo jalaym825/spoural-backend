e6c142f38f4c06373ae665f3abe658e1bb17d0f0
Jalay Movaliya <jalaym825@gmail.com>
2024-04-12 21:04:57 +0530

    Update build script to include Prisma generation and migration deployment

--- prisma/generated/client/edge.js
+++ prisma/generated/client/edge.js
@@ -318,7 +318,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
