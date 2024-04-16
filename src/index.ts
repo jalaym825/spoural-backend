27393e891efcc574dfff657b9776266f1435cf1b
Jalay Movaliya <jalaym825@gmail.com>
2024-04-16 14:09:40 +0530

    Add @socket.io/cluster-adapter dependency and update index.ts to implement clustering for socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,81 @@
-require('events').EventEmitter.defaultMaxListeners = 15; // or a value appropriate for your application
+import cluster, { Worker } from "cluster";
+import os from 'os';
 import prisma from './utils/prisma';
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
 import helmet from 'helmet';
 import routes from './router';
 import cookieParser from 'cookie-parser';
+import { createAdapter, setupPrimary } from "@socket.io/cluster-adapter";
 import 'colors';
 const config = require('../config.json');
 
-const app = express();
-const server = http.createServer(app);
+const numCPUs = 1;
+// const numCPUs = os.cpus().length;
 
-app.use(cors({
-    origin: '*',
-}));
-app.use(express.json());
-app.use(helmet());
-app.use(cookieParser());
+if (cluster.isPrimary) {
+    console.log(`Master ${process.pid} is running`);
 
-
-const io = new Server(server, {
-    cors:{
-        origin: "*",
-        methods: ["GET", "POST"]
+    // Fork workers
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
     }
-});
 
-io.on("connection", (socket) => {
-    console.log(`user connected socketId: ${socket.id}`);
-    socket.on("runsUpdated", data => {
-        console.log(data)
-        socket.broadcast.emit("updateRuns");
-    })
-    socket.on("disconnect", () => {
-        console.log("user disconnected");
+    setupPrimary();
+    
+    // Ensure 'cluster' is indeed the Cluster module
+    cluster.on('exit', (worker: Worker, code: number, signal: string) => {
+        console.log(`Worker ${worker.process.pid} died`);
+        cluster.fork();
     });
-})
+} else {
+    // Worker process
+    console.log(`Worker ${process.pid} started`);
 
-const port = config.PORT || 3000;
+    const app = express();
+    const server = http.createServer(app);
 
-server.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-    prisma.$connect().then(() => {
-        console.log('Connected to database');
+    app.use(cors({
+        origin: '*',
+    }));
+    app.use(express.json());
+    app.use(helmet());
+    app.use(cookieParser());
+
+
+    const io = new Server(server, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+        },
+        adapter: createAdapter()
+    });
+
+    io.on("connection", (socket) => {
+        console.log(`user connected socketId: ${socket.id}`);
+        socket.on("runsUpdated", (match: any) => { // Adjust the type as per your data structure
+            console.log(match)
+            socket.broadcast.emit("updateRuns", match);
+        })
+        socket.on("disconnect", () => {
+            console.log("user disconnected");
+        });
     })
-    routes(app);
-});
 
-app.get("/", (req: Request, res: Response) => {
-    // res.json({ message: "Hello World" });
-    res.json("OK");
-})
+    const port = config.PORT || 3000;
+
+    server.listen(port, () => {
+        console.log(`Worker ${process.pid} is listening on port ${port}`);
+        prisma.$connect().then(() => {
+            console.log('Connected to database');
+        })
+        routes(app);
+    });
+
+    app.get("/", (req: Request, res: Response) => {
+        // res.json({ message: "Hello World" });
+        res.json("OK");
+    })
+}
